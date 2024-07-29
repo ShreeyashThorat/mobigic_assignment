d36766fe698ede5bba287dbf2a1435af65512d75
ShreeyashThorat <shreeyashthorat5@gmail.com>
2024-07-29 12:15:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,58 @@
+import express from "express";
+import { rateLimit } from "express-rate-limit";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+// import userRoutes from "./routes/user_routes.js";
+
+dotenv.config();
+const app = express();
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+}));
+
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 500,
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: (_, __, ___, options) => {
+        throw (
+            500,
+            `There are too many requests.`
+        );
+    },
+});
+
+app.use(limiter);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use("/restapi/v1", express.Router().get("/api", async (req, res) => {
+    return res.status(200).json({ status: true, code: 200, message: "Success" });
+}))
+// app.use("/restapi/v1", userRoutes);
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || 'Internal Server Error';
+    res.status(err.statusCode).json({
+        message: err.message,
+    });
+});
+
+const startServer = async () => {
+    try {
+        // await db.getConnection();
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on http://localhost:${process.env.PORT}`);
+        });
+    } catch (err) {
+        console.error('Error connecting to database:', err);
+    }
+};
+
+startServer();
